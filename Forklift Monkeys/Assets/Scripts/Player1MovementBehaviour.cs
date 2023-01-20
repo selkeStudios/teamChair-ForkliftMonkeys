b280fa5485e67000c97e08c4079ebb5fcbe5ad73
JeremiahPeters <jcpeters@mail.bradley.edu>
2023-01-20 15:37:52 -0600

    groundwork for steering wheel input
    doesn't do anything yet

---
+++ Forklift Monkeys/Assets/Scripts/Player1MovementBehaviour.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1MovementBehaviour : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
