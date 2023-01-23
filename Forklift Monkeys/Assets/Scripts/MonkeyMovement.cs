9c52059954e2ea506e08f981f09b7591b1454136
Selke Studios <selkestudioss@gmail.com>
2023-01-22 20:20:27 -0600

    Input testing

--- Forklift Monkeys/Assets/Scripts/MonkeyMovement.cs
+++ Forklift Monkeys/Assets/Scripts/MonkeyMovement.cs
@@ -1,13 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MonkeyMovement : MonoBehaviour
 {
+    public InputActions controls;
+
     public CharacterController controller;
 
     public float speed = 6f;
     public float rotationSpeed = 6f;
+
+    private void Awake()
+    {
+        controls.Player1.Accelerate.performed += _ => Shoot();
+    }
+    
+    void Shoot()
+    {
+        print("Accelerate");
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+
     // Update is called once per frame
     void Update()
     {
