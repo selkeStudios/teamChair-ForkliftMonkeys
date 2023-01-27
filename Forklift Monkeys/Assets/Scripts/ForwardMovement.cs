e5704bacdfa35d6b6b61af1076d48976910b9dab
JeremiahPeters <jcpeters@mail.bradley.edu>
2023-01-27 15:23:37 -0600

    yea

--- Forklift Monkeys/Assets/Scripts/ForwardMovement.cs
+++ Forklift Monkeys/Assets/Scripts/ForwardMovement.cs
@@ -12,6 +12,36 @@ public class ForwardMovement : MonoBehaviour
     Vector3 moveDirection;
     Rigidbody rb;
 
+
+    //input system stuff
+    InputActions controls;
+    public float move;
+    public bool APressed = false;
+    public bool BPressed = false;
+    public bool YPressed = false;
+    public bool XPressed = false;
+
+    private void Awake()
+    {
+        //input system stuff
+        controls = new InputActions();
+
+        //left is 1, right is -1
+        controls.Player1.Wheel.performed += ctx => move = ctx.ReadValue<float>();
+
+        controls.Player1.Accelerate.performed += ctx => APressed = true;
+        controls.Player1.Accelerate.canceled += ctx => APressed = false;
+
+        controls.Player1.Reverse.performed += ctx => BPressed = true;
+        controls.Player1.Reverse.canceled += ctx => BPressed = false;
+
+        controls.Player1.UseItem.performed += ctx => XPressed = true;
+        controls.Player1.UseItem.canceled += ctx => XPressed = false;
+
+        controls.Player1.Horn.performed += ctx => YPressed = true;
+        controls.Player1.Horn.canceled += ctx => YPressed = false;
+    }
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -30,9 +60,40 @@ public class ForwardMovement : MonoBehaviour
 
     private void GetInput()
     {
-        verticalInput = Input.GetAxisRaw("Vertical");
+        //old movement (and for keyboard)
+        //verticalInput = Input.GetAxisRaw("Vertical");
+        //hInput = Input.GetAxisRaw("Horizontal");
 
-        hInput = Input.GetAxisRaw("Horizontal");
+        hInput = move;
+
+        if (APressed == true)
+        {
+            //accelerate
+            verticalInput = 1;
+            Debug.Log("you pressed A");
+        }
+        else if (BPressed == true)
+        {
+            //reverse
+            verticalInput = -1;
+            Debug.Log("you pressed B");
+        }
+        else
+        {
+            verticalInput = 0;
+        }
+
+        if (XPressed == true)
+        {
+            //accelerate
+            Debug.Log("use item");
+        }
+        if (YPressed == true)
+        {
+            //reverse
+            Debug.Log("*horn noises*");
+        }
+
     }
 
     private void MovePlayer()
@@ -44,5 +105,14 @@ public class ForwardMovement : MonoBehaviour
 
 
         rb.AddForce(moveDirection.normalized * moveSpeed, ForceMode.Force);
+    }
+
+    private void OnEnable()
+    {
+        controls.Player1.Enable();
+    }
+    private void OnDisable()
+    {
+        controls.Player1.Disable();
     }
 }
