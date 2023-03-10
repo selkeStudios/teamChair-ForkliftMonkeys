028b2f3ec67c224dd2dcd0cb436d9c31af006f69
Marissa Moser <marissalmoser@gmail.com>
2023-03-10 13:48:56 -0600

    eh

--- Forklift Monkeys/Assets/Scripts/GameManager.cs
+++ Forklift Monkeys/Assets/Scripts/GameManager.cs
@@ -7,12 +7,15 @@ public class GameManager : MonoBehaviour
 {
     public GameObject PlayerPrefab;
 
-    //static PlayerInput Instantiate(GameObject PlayerPrefab, int playerIndex, string controlScheme = null, int splitScreenIndex = 0, InputDevice pairWithDevice = null);
+    static PlayerInput Instantiate(GameObject PlayerPrefab, int playerIndex, string controlScheme = null, int splitScreenIndex = 0, InputDevice pairWithDevice = null)
+    {
+        return PlayerPrefab.GetComponent<PlayerInput>();
+    }
 
 
     void Start()
     {
-         
+        Instantiate(PlayerPrefab, 0, null, 0, null);
     }
 
     //public PlayerInput JoinPlayer(int playerIndex, int splitScreenIndex, string controlScheme = null, params InputDevice[] pairWithDevices)
