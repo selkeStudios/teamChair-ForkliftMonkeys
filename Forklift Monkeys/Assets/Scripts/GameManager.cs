94bdb72ffb871cf096b00f9636c7d63e128db7c1
Selke Studios <selkestudioss@gmail.com>
2023-03-13 23:40:40 -0500

    A lot of fixing

--- Forklift Monkeys/Assets/Scripts/GameManager.cs
+++ Forklift Monkeys/Assets/Scripts/GameManager.cs
@@ -6,13 +6,55 @@ using UnityEngine.InputSystem;
 public class GameManager : MonoBehaviour
 {
     public GameObject PlayerObject;
+    public PlayerInputManager pIM;
+    public Transform[] playerSpawnPts;
+
     void Start()
     {
+        joinANewPlayer();
+        /*
+        for (int i = 0; i < 4; ++i)
+        {
+            joinANewPlayer();
+            //The first parameter controls which screen the respective player is on
+
+            //print(i);
+        }
+        */
+        //pIM.JoinPlayer(1, 1, null);
+        /*
+        for (int i = 0; i < 2; i++)
+        {
+            pIM.JoinPlayer(i, i, "test", inputDevice);
+            //pIM.playerPrefab.transform.position = playerSpawnPts[i].position;
+        }
+        */
         //JoinPlayer(0, 0, );
+    }
+
+    private void Update()
+    {
+        /*
+        if(Input.GetKeyDown(KeyCode.L))
+        {
+            joinANewPlayer();
+        }
+        */
+    }
+
+    public void joinANewPlayer()
+    {
+        if(pIM.playerCount < pIM.maxPlayerCount)
+        {
+            int playerIndex = pIM.playerCount;
+            pIM.JoinPlayer(playerIndex);
+
+            pIM.playerPrefab.gameObject.transform.position = playerSpawnPts[playerIndex].position;
+        }
     }
 
     //public PlayerInput JoinPlayer(int playerIndex, int splitScreenIndex, string controlScheme = null, params InputDevice[] pairWithDevices)
     //{
-        //return GameObject;
+    //return GameObject;
     //}
 }
