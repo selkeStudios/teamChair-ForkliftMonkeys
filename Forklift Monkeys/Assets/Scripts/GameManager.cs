61b3e3b51ce11676c4e20790c518ff5c2671b3a0
Selke Studios <selkestudioss@gmail.com>
2023-03-24 14:54:29 -0500

    Update GameManager.cs

--- Forklift Monkeys/Assets/Scripts/GameManager.cs
+++ Forklift Monkeys/Assets/Scripts/GameManager.cs
@@ -20,7 +20,6 @@ public class GameManager : MonoBehaviour
             pIM.playerPrefab.GetComponent<ForwardMovement>().RespawnPoint = playerSpawnPts[i].position;
         }
         
-
         //JoinFourPlayers();
 
         /*
