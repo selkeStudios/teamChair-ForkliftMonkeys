9747c9561e3a1d0d2d1d5ebf7f4f5ebf20ae8c98
Selke Studios <selkestudioss@gmail.com>
2023-04-14 15:42:26 -0500

    UI Implementation

--- Forklift Monkeys/Assets/Scripts/mainMenuBehaviour.cs
+++ Forklift Monkeys/Assets/Scripts/mainMenuBehaviour.cs
@@ -7,6 +7,9 @@ public class mainMenuBehaviour : MonoBehaviour
 {
     public Animator monkeyTransition;
 
+    public Animator tutorialAnim1;
+    public Animator tutorialAnim2;
+
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.R))
@@ -22,6 +25,24 @@ public class mainMenuBehaviour : MonoBehaviour
 
     public void LoadScene()
     {
+        StartCoroutine(tutorialOneShow());
+    }
+
+    public IEnumerator tutorialOneShow()
+    {
+        tutorialAnim1.gameObject.SetActive(true);
+        yield return new WaitForSeconds(8f);
+        tutorialAnim1.SetTrigger("fadee");
+        yield return new WaitForSeconds(1f);
+        StartCoroutine(tutorialTwoShow());
+    }
+
+    public IEnumerator tutorialTwoShow()
+    {
+        tutorialAnim2.gameObject.SetActive(true);
+        yield return new WaitForSeconds(8f);
+        tutorialAnim2.SetTrigger("fadee");
+        yield return new WaitForSeconds(1f);
         SceneManager.LoadScene("Testing");
     }
 }
