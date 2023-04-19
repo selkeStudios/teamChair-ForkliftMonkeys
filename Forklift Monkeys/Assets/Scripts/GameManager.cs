a3396066bc6d50160ac549c5e7e05d1dbb71ec0f
Selke Studios <selkestudioss@gmail.com>
2023-04-19 13:43:08 -0500

    Final Polishing

--- Forklift Monkeys/Assets/Scripts/GameManager.cs
+++ Forklift Monkeys/Assets/Scripts/GameManager.cs
@@ -11,7 +11,7 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
-        
+        /*
         for (int i = 0; i < 4; i++)
         {
             PlayerInput.Instantiate(PlayerObject, i, null, -1, pairWithDevice: Keyboard.current);
@@ -20,11 +20,11 @@ public class GameManager : MonoBehaviour
             pIM.playerPrefab.transform.position = playerSpawnPts[i].position;
             pIM.playerPrefab.GetComponent<ForwardMovement>().RespawnPoint = playerSpawnPts[i].position;
         }
+        */
         
         //PlayerInput.Instantiate(PlayerObject, 0, null, -1, pairWithDevice: Keyboard.current);
         //PlayerInput.Instantiate(PlayerObject, 1, null, -1, pairWithDevice: Gamepad.all[0]);
 
-        /*
         for (int i = 0; i < 4; i++)
         {
             //spawns in the four players paired to their device
@@ -34,6 +34,5 @@ public class GameManager : MonoBehaviour
             pIM.playerPrefab.transform.position = playerSpawnPts[i].position;
             pIM.playerPrefab.GetComponent<ForwardMovement>().RespawnPoint = playerSpawnPts[i].position;
         }
-        */
     }
 }
